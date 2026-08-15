98ec74e56b1d8bf5c423773d342cce7e03674b61
Dingo <oferdottan@gmail.com>
2026-08-15 12:29:37 +0300

    fix controller empty binding

--- Subnautica/Keybinds.cs
+++ Subnautica/Keybinds.cs
@@ -4,19 +4,21 @@ namespace SubnauticaAutosave
 {
     internal class Keybinds
     {
-    	private const string DefaultBinding = "<Keyboard>/f9";
-    	private const string DebugDefaultBinding = "<Keyboard>/leftBracket";
+    	private const string QuicksaveDefault = "<Keyboard>/f9";
+    	private const string DebugAutosaveDefault = "<Keyboard>/leftBracket";
 
         internal GameInput.Button Quicksave = EnumHandler.AddEntry<GameInput.Button>("SubnauticaAutosaveQuicksave")
                     .CreateInput("QuicksaveKey".Translate(), "Tooltip_QuicksaveKey".Translate())
-                    .WithKeyboardBinding(primaryBindingPath: DefaultBinding, secondaryBindingPath: null)
+                    .WithKeyboardBinding(QuicksaveDefault)
+                    .WithControllerBinding("None")
                     .AvoidConflicts(GameInput.Device.Keyboard)
                     .WithCategory(ModPlugin.modName);
 
 #if DEBUG
         internal GameInput.Button DebugAutosaveTrigger = EnumHandler.AddEntry<GameInput.Button>("SubnauticaAutosaveDebugAutosave")
                 .CreateInput("Debug: Trigger Autosave", "Forces TryExecuteAutosave() for testing.")
-                .WithKeyboardBinding(DebugDefaultBinding)
+                .WithKeyboardBinding(DebugAutosaveDefault)
+                .WithControllerBinding("None")
                 .AvoidConflicts(GameInput.Device.Keyboard)
                 .WithCategory(ModPlugin.modName);
 #endif
